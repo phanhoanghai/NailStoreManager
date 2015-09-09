860d1ad4c22b3fd6068f83ce5b85269f7ca9a9c3
phanhoanghai <phanhoanghai90@gmail.com>
2015-09-09 16:10:46 +0700

    fix some err

---
+++ app/src/main/gen/app1/phh/com/nailstoremanager/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app1.phh.com.nailstoremanager;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- app/src/main/java/app1/phh/com/nailstoremanager/Jobs/DisplayJob.java
+++ app/src/main/java/app1/phh/com/nailstoremanager/Jobs/DisplayJob.java
@@ -1,11 +1,18 @@
 package app1.phh.com.nailstoremanager.Jobs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import app1.phh.com.nailstoremanager.Contact.DisplayContact;
 import app1.phh.com.nailstoremanager.R;
 import app1.phh.com.nailstoremanager.database.SqlHelper;
 
@@ -14,7 +21,7 @@ import app1.phh.com.nailstoremanager.database.SqlHelper;
  */
 public class DisplayJob extends Activity {
     private SqlHelper mydb;
-
+    private ListView listViewJobs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +32,29 @@ public class DisplayJob extends Activity {
 
         this.mapComponent();
 
+        ArrayList arrayListJob = mydb.getAllJobs();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayListJob);
+
+        listViewJobs.setAdapter(arrayAdapter);
+        listViewJobs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(), DisplayContact.class);
+
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
 
     }
 
     private void mapComponent(){
+        listViewJobs = (ListView)findViewById(R.id.listViewJobs);
     }
 
-    private ArrayList<String> getTableValue() {
-        ArrayList<String> arrayList = new ArrayList<String>();
-        try{
-
-        }catch (Exception ex){
-
-        }
-        return arrayList;
-    }
 }
